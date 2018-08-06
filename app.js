a95aa4a5718e2219d3b40d023f604cd9cadc914a
pyeon <mail-j@naver.com>
2018-08-06 16:20:48 +0900

    readme.md added

---
+++ app.js
@@ -0,0 +1,15 @@
+function fizzBuzz() {
+  for (i=1; i<=100; i++) {
+    if(i%3 == 0  && i%5 == 0) {
+      console.log("");
+    } else if (i%3 == 0) {
+      console.log("");
+    } else if (i%5 == 0) {
+      console.log("");
+    } else {
+      console.log("");
+    }
+  }
+}
+
+console.log(fizzBuzz());
